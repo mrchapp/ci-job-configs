ecfd6d517f43422ab2808777e09a36eb2b89b256
Aníbal Limón <anibal.limon@linaro.org>
2021-03-10 11:38:58 -0600

    lt-qcom-linux-test: Update get_latest_testimage.py to get desktop image
    Now desktop testimage is generated from different CI job, update to get
    it.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I7f8c08b7396ffd4b0ccaf21ffbc512d68f6b7c21

--- lt-qcom-linux-test/get_latest_testimage.py
+++ lt-qcom-linux-test/get_latest_testimage.py
@@ -21,31 +21,36 @@ def get_image_url(page, base_url, rex_str):
             break
     return url
 
-def main(url='https://snapshots.linaro.org/member-builds/qcomlt/testimages/arm64/',
-         job_url='https://ci.linaro.org/job/lt-qcom-linux-testimages/',
-         output='output.log'):
-
+def get_base_url(url, job_url):
     f = urllib.request.urlopen(job_url + "lastSuccessfulBuild/buildNumber")
     last_build = int(f.read().decode("utf-8"))
-
     url = '%s/%d/' % (url, last_build)
     f = urllib.request.urlopen(url)
     page = f.read().decode("utf-8")
     base_url_p = urllib.parse.urlparse(url)
     base_url = "%s://%s" % (base_url_p.scheme, base_url_p.netloc)
+    return (page, base_url)
 
+def main(url='https://snapshots.linaro.org/member-builds/qcomlt/testimages/arm64/',
+         job_url='https://ci.linaro.org/job/lt-qcom-linux-testimages/',
+         desktop_url='https://snapshots.linaro.org/member-builds/qcomlt/testimages-desktop/arm64/',
+         desktop_job_url='https://ci.linaro.org/job/lt-qcom-linux-testimages-desktop/',
+         output='output.log'):
+
+    (page, base_url) = get_base_url(url, job_url)
     ramdisk_url = get_image_url(page, base_url, 'initramfs-test-image-.*\.rootfs\.cpio\.gz$')
     validate_url(ramdisk_url)
     rootfs_url = get_image_url(page, base_url, 'rpb-console-image-test-.*\.rootfs\.img\.gz$')
     validate_url(rootfs_url)
-    rootfs_desktop_url = get_image_url(page, base_url, 'rpb-desktop-image-test-.*\.rootfs\.img\.gz$')
-    validate_url(rootfs_desktop_url)
-
-    print('Writting output to %s' % output)
     print('RAMDISK_URL=%s' % ramdisk_url)
     print('ROOTFS_URL=%s' % rootfs_url)
+
+    (page, desktop_base_url) = get_base_url(desktop_url, desktop_job_url)
+    rootfs_desktop_url = get_image_url(page, desktop_base_url, 'rpb-desktop-image-test-.*\.rootfs\.img\.gz$')
+    validate_url(rootfs_desktop_url)
     print('ROOTFS_DESKTOP_URL=%s' % rootfs_desktop_url)
 
+    print('Writting output to %s' % output)
     with open(output, 'w') as f:
         f.write("RAMDISK_URL=" + ramdisk_url + '\n')
         f.write("ROOTFS_URL=" + rootfs_url + '\n')
