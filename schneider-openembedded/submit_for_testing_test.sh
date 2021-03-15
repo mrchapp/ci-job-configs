cd944f6c452ed60719cf9a77c2d26918118a1f2c
Milan Stevanovic <milan.stevanovic@se.com>
2021-03-15 08:15:44 +0000

    se: add lava tests with new image names
    Signed-off-by: Milan Stevanovic <milan.stevanovic@se.com>
    Change-Id: I43d5598070afca7b8e2ff225b3e6d1a0977ecc87

--- schneider-openembedded/submit_for_testing_test.sh
+++ schneider-openembedded/submit_for_testing_test.sh
@@ -6,3 +6,93 @@ pushd configs
 git log -1
 popd
 
+# Used by DB410C's template:
+export RESIZE_ROOTFS=${RESIZE_ROOTFS:-}
+
+templates_common_minimal=( prod-image.yaml     )
+templates_common_normal=(  ${templates_common_minimal[@]} )
+if [[ "${IMAGES}" == *dev-image* ]]; then
+templates_common_normal=(  ${templates_common_normal[@]} dev-image.yaml )
+fi
+templates_common_network=( ${templates_common_normal[@]}  )
+templates_common_full=(    ${templates_common_network[@]}  ltp-ptest.yaml )
+
+templates_soca9_minimal=
+templates_soca9_normal=( \
+	${templates_soca9_minimal[@]}
+	lava-multinode-soca9-j21.yaml
+	lava-multinode-soca9-j24-mtu1508.yaml
+)
+templates_soca9_network=( \
+	${templates_soca9_normal[@]}
+	lava-multinode-soca9-j17.yaml
+	lava-multinode-soca9-j22.yaml
+	lava-multinode-soca9-j23.yaml
+	lava-multinode-soca9-j24.yaml
+	lava-multinode-soca9-j22-mtu1508.yaml
+	lava-multinode-soca9-j23-mtu1508.yaml
+)
+templates_soca9_full=(   ${templates_soca9_network[@]} )
+
+templates_rzn1d_minimal=
+templates_rzn1d_normal=( \
+	${templates_rzn1d_minimal[@]}
+	lava-multinode-rzn1d-j21.yaml
+	lava-multinode-rzn1d-j24-mtu1508.yaml
+)
+
+templates_rzn1d_network=( \
+	${templates_rzn1d_normal[@]}
+	lava-multinode-rzn1d-j17.yaml
+	lava-multinode-rzn1d-j22.yaml
+	lava-multinode-rzn1d-j23.yaml
+	lava-multinode-rzn1d-j24.yaml
+	lava-multinode-rzn1d-j22-mtu1508.yaml
+	lava-multinode-rzn1d-j23-mtu1508.yaml
+)
+templates_rzn1d_full=(   ${templates_rzn1d_network[@]} )
+
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	templates_minimal=( ${templates_common_minimal[@]} ${templates_rzn1d_minimal[@]} )
+	templates_normal=(  ${templates_common_normal[@]}  ${templates_rzn1d_normal[@]} )
+	templates_network=( ${templates_common_network[@]} ${templates_rzn1d_network[@]} )
+	templates_full=(    ${templates_common_full[@]}    ${templates_rzn1d_full[@]} )
+else
+	templates_minimal=( ${templates_common_minimal[@]} ${templates_soca9_minimal[@]} )
+	templates_normal=(  ${templates_common_normal[@]}  ${templates_soca9_normal[@]} )
+	templates_network=( ${templates_common_network[@]} ${templates_soca9_network[@]} )
+	templates_full=(    ${templates_common_full[@]}    ${templates_soca9_full[@]} )
+fi
+
+case $TEST_LEVEL in
+	"none" | "0")
+		templates=()
+		;;
+	"minimal" | "minimum" | "min" | "1")
+		templates=( ${templates_minimal[@]} )
+		;;
+	"normal" | "2")
+		templates=( ${templates_normal[@]} )
+		;;
+	"network" | "3")
+		templates=( ${templates_network[@]} )
+		;;
+	*)
+		templates=( ${templates_full[@]} )
+		;;
+esac
+
+for template in ${templates[@]};
+do
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions-test/ \
+	  --template-names ${template}
+done
+
