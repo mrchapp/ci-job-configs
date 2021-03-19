7b47e47f2c001d1f54e7ca30634a3d7e062cb3a6
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2021-03-19 12:44:42 +0100

    lt-qcom-linux-aic100: fixup build dependencies
    Change-Id: I9472a7e6e0024422808bd476fad40174104f16e3
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- lt-qcom-linux/aic100.sh
+++ lt-qcom-linux/aic100.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -ex
+
+sudo apt update
+sudo apt install -y ccache bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev lz4 libelf-dev libssl-dev build-essential
+
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
 tar xf linux-${KERNEL_VERSION}.tar.xz
 cd linux-${KERNEL_VERSION}
@@ -17,5 +21,6 @@ make EXTRAVERSION=-050401-generic -j$(nproc) rpm-pkg
 
 cd ..
 ls -ltr
+ls -ltr ~/rpmbuild/RPMS
 
 
