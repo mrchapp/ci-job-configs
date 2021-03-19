673733ffb92eb29e5ff1e696253189c922e3813b
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2021-03-19 16:30:11 +0100

    lt-qcom-linux-aic100: publish deb and rpm packages
    Change-Id: I58293e402a50f07b0ab32bdf96ac5330af382339
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- lt-qcom-linux/aic100.sh
+++ lt-qcom-linux/aic100.sh
@@ -5,6 +5,8 @@ set -ex
 sudo apt update
 sudo apt install -y ccache bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev lz4 libelf-dev libssl-dev build-essential rsync rpm
 
+mkdir -p ${WORKSPACE}/out
+
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
 tar xf linux-${KERNEL_VERSION}.tar.xz
 cd linux-${KERNEL_VERSION}
@@ -13,14 +15,11 @@ cd linux-${KERNEL_VERSION}
 make mrproper
 wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/${DEB_CONFIG} -O .config
 make EXTRAVERSION=-050401-generic -j$(nproc) deb-pkg
+cp ../*.deb ${WORKSPACE}/out/
 
 # Build rpm packages
 make mrproper
 wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/${RPM_CONFIG} -O .config
 make EXTRAVERSION=-050401-generic -j$(nproc) rpm-pkg
-
-cd ..
-ls -ltr
-ls -ltr ~/rpmbuild/RPMS
-
+cp ~/rpmbuild/RPMS/x86_64/*.rpm ${WORKSPACE}/out/
 
