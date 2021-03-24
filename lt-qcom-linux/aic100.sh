7f98d86824aa0fd4107e3cbc0874c46819a198be
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2021-03-24 16:15:30 +0100

    lt-qcom-linux-aic100: use job templates
    Third job implementation!
    
    The user only needs the .deb or the .rpm. So instead of trying to have
    a single job that builds both of them, let's use a template to
    generate 2 different independent jobs.
    
    Change-Id: Ifcbd7bbb83ff35bfe77decabe921a0576709359b
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- lt-qcom-linux/aic100.sh
+++ lt-qcom-linux/aic100.sh
@@ -9,29 +9,19 @@ wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
 tar xf linux-${KERNEL_VERSION}.tar.xz
 cd linux-${KERNEL_VERSION}
 
-for node in ${NODE_LABELS}; do
-    case $node in
-	# Build deb packages for 18.04
-	docker-bionic-amd64|docker-buster-amd64)
-	    sudo apt update
-	    sudo apt install -y bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev lz4 libelf-dev libssl-dev build-essential rsync
-	    make mrproper
-	    wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/config-5.4.1-050401-generic -O .config
-	    make EXTRAVERSION=-050401-generic -j$(nproc) deb-pkg
-	    # .deb files are built in ../ so in WORKSPACE already
-	    ;;
+if [ "${DISTRO}" == "bionic" ]; then
+    sudo apt update
+    sudo apt install -y bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev lz4 libelf-dev libssl-dev build-essential rsync
+    make mrproper
+    wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/config-5.4.1-050401-generic -O .config
+    make EXTRAVERSION=-050401-generic -j$(nproc) deb-pkg
+    cp ../*.deb ${WORKSPACE}/out
 
-	# Build rpm packages for Centos7
-	docker-centos7-amd64)
-	    sudo yum check-update
-	    sudo yum install -y git which gcc ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel vim rpm-build rsync
-	    make mrproper
-	    wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/config-5.4.1-1.el7.elrepo.x86_64 -O .config
-	    make EXTRAVERSION=-1.el7.elrepo.x86_64 -j$(nproc) rpm-pkg
-	    cp ~/rpmbuild/RPMS/x86_64/*.rpm ${WORKSPACE}/
-	    ;;
-
-	*)
-	    echo "Unsupported build host: $node"
-    esac
-done
+elif [ "${DISTRO}" == "centos7" ]; then
+    sudo yum check-update
+    sudo yum install -y git which gcc ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel vim rpm-build rsync
+    make mrproper
+    wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/config-5.4.1-1.el7.elrepo.x86_64 -O .config
+    make EXTRAVERSION=-1.el7.elrepo.x86_64 -j$(nproc) rpm-pkg
+    cp ~/rpmbuild/RPMS/x86_64/*.rpm ${WORKSPACE}/out
+fi
