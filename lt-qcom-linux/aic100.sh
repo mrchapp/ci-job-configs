547a34e4989dd6dbc5b270dab6783f670269acdd
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2021-03-24 16:24:59 +0100

    lt-qcom-linux/aic100: fix lz4 package name on bionic
    Change-Id: I635b908ba3861e6ab6b50f532586fab47a49b726
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- lt-qcom-linux/aic100.sh
+++ lt-qcom-linux/aic100.sh
@@ -11,7 +11,7 @@ cd linux-${KERNEL_VERSION}
 
 if [ "${DISTRO}" == "bionic" ]; then
     sudo apt update
-    sudo apt install -y bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev lz4 libelf-dev libssl-dev build-essential rsync
+    sudo apt install -y bc kmod cpio dpkg-dev wget flex bison bc kmod cpio libssl-dev liblz4-tool libelf-dev libssl-dev build-essential rsync
     make mrproper
     wget https://git.linaro.org/ci/job/configs.git/plain/lt-qcom-linux/aic100/config-5.4.1-050401-generic -O .config
     make EXTRAVERSION=-050401-generic -j$(nproc) deb-pkg
