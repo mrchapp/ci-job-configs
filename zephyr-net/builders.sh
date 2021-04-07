60bd296f7ceba2652903a7007a0c57cb9ac65c48
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-04-07 18:14:35 +0300

    zephyr-net: Re-enable USE_CCACHE which was disabled for debugging before.
    Change-Id: I0d41de2582ad9aec9c3909021f778c61aabbfc53
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- zephyr-net/builders.sh
+++ zephyr-net/builders.sh
@@ -94,7 +94,7 @@ export LANG ZEPHYR_BASE PATH
 CCACHE_DIR="${HOME}/srv/ccache-zephyr/${BRANCH}"
 CCACHE_UNIFY=1
 CCACHE_SLOPPINESS=file_macro,include_file_mtime,time_macros
-USE_CCACHE=0
+USE_CCACHE=1
 export CCACHE_DIR CCACHE_UNIFY CCACHE_SLOPPINESS USE_CCACHE
 env |grep '^ZEPHYR'
 mkdir -p "${CCACHE_DIR}"
