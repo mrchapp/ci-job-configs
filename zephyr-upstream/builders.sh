5de4624aa57730a62134efa334439de8b1e2ee43
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-04-09 17:22:57 +0300

    zephyr-upstream: Install protobuf-compiler needed for recent sample
    Need for recently added samples/modules/nanopb.
    
    Change-Id: Ic994fd67d6c64e998b672dac6b5af491b762f3cf
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- zephyr-upstream/builders.sh
+++ zephyr-upstream/builders.sh
@@ -11,7 +11,7 @@ sudo apt-get -q=2 update
 sudo apt-get -q=2 -y install git ninja-build g++ g++-multilib gperf python3-ply \
     gcc-arm-none-eabi rsync device-tree-compiler \
     python3-pip python3-serial python3-setuptools python3-wheel \
-    python3-requests util-linux rename srecord
+    python3-requests util-linux rename srecord protobuf-compiler
 
 set -ex
 
