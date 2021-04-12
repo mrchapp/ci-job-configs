e97c4c703ea43f5887136b857d7254b6f3f6cedb
Haojian Zhuang <haojian.zhuang@linaro.org>
2021-04-12 11:41:36 +0800

    uadk: remove v1 from build process
    Since v1 and v2 are forced to build in the same binary, there's no
    requirement to build v1 and v2 separately. So remove v1 from build.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>
    Change-Id: I073f5c7ee63e40cea6ec8644a5fb676930ff05fd

--- uadk/builders.sh
+++ uadk/builders.sh
@@ -14,15 +14,6 @@ echo "#${BUILD_NUMBER}-${ghprbActualCommit:0:8}" > ${WORKSPACE}/version.txt
 cd ${WORKSPACE}/uadk
 autoreconf -vfi
 
-# shared build for v1
-./conf.sh --with-uadk_v1 && make -j$(nproc)
-make install DESTDIR=${WORKSPACE}/uadk-shared-v1 && make clean
-sudo \
-  LD_LIBRARY_PATH=${WORKSPACE}/uadk-shared-v1/usr/local/lib/ \
-  PATH=${WORKSPACE}/uadk-shared-v1/usr/local/bin:${PATH}  \
-  C_INCLUDE_PATH=${WORKSPACE}/uadk-shared-v1/usr/local/include/ \
-  ${WORKSPACE}/uadk/test/sanity_test.sh
-
 # shared build for v2
 ./conf.sh && make -j$(nproc)
 make install DESTDIR=${WORKSPACE}/uadk-shared-v2 && make clean
@@ -30,15 +21,6 @@ sudo \
   LD_LIBRARY_PATH=${WORKSPACE}/uadk-shared-v2/usr/local/lib/ \
   PATH=${WORKSPACE}/uadk-shared-v2/usr/local/bin:${PATH}  \
   C_INCLUDE_PATH=${WORKSPACE}/uadk-shared-v2/usr/local/include/ \
-  ${WORKSPACE}/uadk/test/sanity_test.sh
-
-# static build for v1
-./conf.sh --with-uadk_v1 --static && make -j$(nproc)
-make install DESTDIR=${WORKSPACE}/uadk-static-v1 && make clean
-sudo \
-  LD_LIBRARY_PATH=${WORKSPACE}/uadk-static-v1/usr/local/lib/ \
-  PATH=${WORKSPACE}/uadk-static-v1/usr/local/bin:${PATH}  \
-  C_INCLUDE_PATH=${WORKSPACE}/uadk-static-v1/usr/local/include/ \
   ${WORKSPACE}/uadk/test/sanity_test.sh
 
 # static build for v2
