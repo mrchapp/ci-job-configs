aa79c26ac23c9da52db243da579f6353d57076f2
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-05-14 15:04:08 +0300

    lite-tf-m: Replace git clone of CMSIS with tarball (pack) download
    git clone includes pulling files via git-lfs, which are really needed
    for the TF-M build. Github project currently over git-lfs quota, so
    the clone fails. As we need a particular release anyway, download
    and extract a release pack file instead.
    
    Change-Id: I682dc659b84183969e52a3a090cbe80097c34894
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/build.sh
+++ lite-tf-m/build.sh
@@ -15,7 +15,11 @@ export PATH=${GNUARMEMB_TOOLCHAIN_PATH}/bin:$PATH
 git clone https://git.trustedfirmware.org/trusted-firmware-m.git -b ${BRANCH}
 (cd trusted-firmware-m; git checkout ${GIT_COMMIT})
 git clone --depth 1 https://github.com/ARMmbed/mbed-crypto.git -b mbedcrypto-3.0.1
-GIT_LFS_SKIP_SMUDGE=1 git clone --depth 1 https://github.com/ARM-software/CMSIS_5.git -b 5.5.0
+
+# This will clone files via git-lfs, whhich may hit Github quota.
+# git clone --depth 1 https://github.com/ARM-software/CMSIS_5.git -b 5.5.0
+wget -q https://github.com/ARM-software/CMSIS_5/releases/download/5.5.0/ARM.CMSIS.5.5.0.pack
+unzip ARM.CMSIS.5.5.0.pack -d CMSIS_5
 
 cd trusted-firmware-m
 echo "GIT_COMMIT_ID=$(git rev-parse --short=8 HEAD)" > ${WORKSPACE}/env_var_parameters
