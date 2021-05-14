64f7d9faac2e0d557e71d58b334f9d6c5a779cc4
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2021-05-14 12:36:59 +0200

    ldcg-python-manylinux-tensorflow: more versions
    On start of a job we can choose which versions we want to build:
    - 1.15.x
    - 2.4.x
    - 2.5.x
    - nightly (git HEAD)
    
    Each of them is defined in ansible/vars/vars.yml with proper versions of
    required components.
    
    This way we can skip building old branches (1.15, 2.4) when we update
    2.5.x to newer release.
    
    It also allows to run nightly but it is not yet automated.
    
    Change-Id: Ia8c98e3da7e95dde275b33d9276b63cb9c450256

--- ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
+++ ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
@@ -9,10 +9,8 @@ cd /tmp/workspace
 
 # remove wheels and wheelhouse/ from previous jobs
 # we do it here as they are root:root
-rm -rf wheel* *.whl cache* configs
+rm -rf wheel* *.whl cache*
 
-git clone --depth 1 https://git.linaro.org/ci/job/configs.git                                                                                                                
-                                                                                                                                                                             
 cd configs/ldcg-python-manylinux-tensorflow/ansible
-                                                                                                                                                                             
+
 ansible-playbook playbooks/build-tf.yml
--- ldcg-python-manylinux-tensorflow/build.sh
+++ ldcg-python-manylinux-tensorflow/build.sh
@@ -5,11 +5,26 @@ set -xe
 cd $WORKSPACE
 
 # remove generated vars and build script
-rm -f *.sh
+rm -f *.sh configs
 
-wget https://git.linaro.org/ci/job/configs.git/plain/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
+git clone --depth 1 https://git.linaro.org/ci/job/configs.git
+
+cp configs/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh .
 
 # 00:01:17.010 /usr/local/bin/manylinux-entrypoint: line 8: /tmp/wheels/build-manylinux2014-wheels.sh: Permission denied
 chmod 755 build-manylinux2014-wheels.sh
+
+if [ "$build115" = "true" ]; then
+    echo '  - "1.15"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+fi
+if [ "$build24" = "true" ]; then
+    echo '  - "2.4"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+fi
+if [ "$build25" = "true" ]; then
+    echo '  - "2.5"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+fi
+if [ "$buildgit" = "true" ]; then
+    echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+fi
 
 docker run -u root -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
