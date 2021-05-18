6c26f3e10b6b8f167ca537c85d60352da8551d33
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2021-05-18 14:17:32 +0200

    ldcg-python-manylinux-tensorflow: append proper file
    Change-Id: Ie79a2321c9d8356d63a7ff2d6c56be770184b66c

--- ldcg-python-manylinux-tensorflow/build.sh
+++ ldcg-python-manylinux-tensorflow/build.sh
@@ -14,16 +14,16 @@ cp configs/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh .
 chmod 755 build-manylinux2014-wheels.sh
 
 if [ "$build115" = "true" ]; then
-    echo '  - "1.15"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+    echo '  - "1.15"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.yml
 fi
 if [ "$build24" = "true" ]; then
-    echo '  - "2.4"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+    echo '  - "2.4"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.yml
 fi
 if [ "$build25" = "true" ]; then
-    echo '  - "2.5"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+    echo '  - "2.5"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.yml
 fi
 if [ "$buildgit" = "true" ]; then
-    echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+    echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.yml
 fi
 
 docker run -u root -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
