0fb0435e386b2db7d0f23b6c4ecc2e74112677fa
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2021-05-18 14:05:01 +0200

    ldcg-python-manylinux-tensorflow: temporary change to cleanup workspace
    Change-Id: Idc0d8d6747050d11780edd7c6b4234286f0ede21

--- ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
+++ ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
@@ -3,13 +3,13 @@
 set -xe
 
 # Ansible is noarch so we can just grab it from x86-64 repo
-yum install -y http://mirror.centos.org/centos/7/configmanagement/x86_64/ansible-29/Packages/a/ansible-2.9.18-1.el7.noarch.rpm
+#yum install -y http://mirror.centos.org/centos/7/configmanagement/x86_64/ansible-29/Packages/a/ansible-2.9.18-1.el7.noarch.rpm
 
 cd /tmp/workspace
 
 # remove wheels and wheelhouse/ from previous jobs
 # we do it here as they are root:root
-rm -rf wheel* *.whl cache*
+rm -rf wheel* *.whl cache* configs
 
 cd configs/ldcg-python-manylinux-tensorflow/ansible
 
--- ldcg-python-manylinux-tensorflow/build.sh
+++ ldcg-python-manylinux-tensorflow/build.sh
@@ -4,26 +4,28 @@ set -xe
 
 cd $WORKSPACE
 
-rm -rf *.sh configs
+rm -rf *.sh
 
-git clone --depth 1 https://git.linaro.org/ci/job/configs.git
+wget https://git.linaro.org/ci/job/configs.git/plain/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh
 
-cp configs/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh .
+#git clone --depth 1 https://git.linaro.org/ci/job/configs.git
+
+#cp configs/ldcg-python-manylinux-tensorflow/build-manylinux2014-wheels.sh .
 
 # 00:01:17.010 /usr/local/bin/manylinux-entrypoint: line 8: /tmp/wheels/build-manylinux2014-wheels.sh: Permission denied
 chmod 755 build-manylinux2014-wheels.sh
 
-if [ "$build115" = "true" ]; then
-    echo '  - "1.15"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
-fi
-if [ "$build24" = "true" ]; then
-    echo '  - "2.4"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
-fi
-if [ "$build25" = "true" ]; then
-    echo '  - "2.5"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
-fi
-if [ "$buildgit" = "true" ]; then
-    echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
-fi
+#if [ "$build115" = "true" ]; then
+#    echo '  - "1.15"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+#fi
+#if [ "$build24" = "true" ]; then
+#    echo '  - "2.4"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+#fi
+#if [ "$build25" = "true" ]; then
+#    echo '  - "2.5"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+#fi
+#if [ "$buildgit" = "true" ]; then
+#    echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars.sh
+#fi
 
 docker run -u root -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
