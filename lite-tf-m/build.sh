0928db67d90621d0d9b5f7b255c4af04901bd98a
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-06-01 15:44:14 +0300

    lite-tf-m: Allow for more fine-grained rev for tf-m-tests.
    Change-Id: Ia14b728f63cbdb6cb68dac49163c5bbc014cd514
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/build.sh
+++ lite-tf-m/build.sh
@@ -15,7 +15,8 @@ export PATH=${GNUARMEMB_TOOLCHAIN_PATH}/bin:$PATH
 git clone https://git.trustedfirmware.org/trusted-firmware-m.git -b ${BRANCH}
 (cd trusted-firmware-m; git checkout ${GIT_COMMIT})
 git clone --depth 1 https://github.com/ARMmbed/mbed-crypto.git -b mbedcrypto-3.0.1
-git clone --depth 1 https://git.trustedfirmware.org/TF-M/tf-m-tests.git -b TF-Mv1.1
+git clone https://git.trustedfirmware.org/TF-M/tf-m-tests.git
+(cd tf-m-tests; git checkout 8a711be291ed7d9ffe6e21ece9a03fc1c7b0357b)
 
 cd trusted-firmware-m
 echo "GIT_COMMIT_ID=$(git rev-parse --short=8 HEAD)" > ${WORKSPACE}/env_var_parameters
