6a164822419e542440a7514ef7e1576126b98adc
Yongqin Liu <yongqin.liu@linaro.org>
2021-06-01 22:27:06 +0800

    lkft: add prefix for the cts/vts version when necessary
    to make it have meaning
    
    Change-Id: I336f01fe603bf9b839b532c093d75bd9dfefe710
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lkft/lava-job-definitions/submit_for_testing-v2.sh
+++ lkft/lava-job-definitions/submit_for_testing-v2.sh
@@ -148,29 +148,29 @@ function submit_jobs_for_config(){
     TEST_VTS_VERSION=$(echo "${TEST_VTS_URL}" | tr -s '/' | sed 's|/$||' | awk -F"/" '{print$(NF)}')
     TEST_CTS_VERSION=$(echo "${TEST_CTS_URL}" | tr -s '/' | sed 's|/$||' | awk -F"/" '{print$(NF)}')
 
-    if echo "${TEST_VTS_VERSION}"|grep '/aosp-master-throttled/'; then
+    if echo "${TEST_VTS_URL}"|grep '/aosp-master-throttled/'; then
         # for aosp master cts/vts packages
         build_number_vts=$(echo "${TEST_VTS_URL}" | tr -s '/' | sed 's|/$||' | awk -F"/" '{print$(NF-1)}')
         TEST_VTS_VERSION="aosp-master-throttled#${build_number_vts}"
-    else
+    elif echo "${TEST_VTS_URL}"|grep '/protected/'; then
         # for EAP cts/vts packages
         f_fingerprint_vts="build_fingerprint_vts.txt"
         download_fingerprint "${TEST_VTS_URL}" "build_fingerprint_vts.txt"
         fingerprint_vts=$(cut -d: -f2 "${f_fingerprint_vts}")
         if [ -n "${f_fingerprint_vts}" ]; then
-            TEST_VTS_VERSION="${fingerprint_vts}"
+            TEST_VTS_VERSION="EAP-Android12#${fingerprint_vts}"
         fi
     fi
 
-    if echo "${TEST_CTS_VERSION}"|grep '/aosp-master-throttled/'; then
+    if echo "${TEST_CTS_URL}"|grep '/aosp-master-throttled/'; then
         build_number_cts=$(echo "${TEST_CTS_URL}" | tr -s '/' | sed 's|/$||' | awk -F"/" '{print$(NF-1)}')
         TEST_CTS_VERSION="aosp-master-throttled#${build_number_cts}"
-    else
+    elif echo "${TEST_CTS_URL}"|grep '/protected/'; then
         f_fingerprint_cts="build_fingerprint_cts.txt"
         download_fingerprint "${TEST_CTS_URL}" "build_fingerprint_cts.txt"
         fingerprint_cts=$(cut -d: -f2 "${f_fingerprint_cts}")
         if [ -n "${f_fingerprint_cts}" ]; then
-            TEST_CTS_VERSION="${fingerprint_cts}"
+            TEST_CTS_VERSION="EAP-Android12#${fingerprint_cts}"
         fi
     fi
 
