b565d532090cac4ce0e0fd6ded6ff0e0f02ee386
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2021-08-30 13:47:12 +0200

    ldcg-python-manylinux-*: remove containers on exit
    Change-Id: I3bae92ebefc47aca76ec69187d28e3c49ba9f558

--- ldcg-python-manylinux-cache/build.sh
+++ ldcg-python-manylinux-cache/build.sh
@@ -16,7 +16,7 @@ echo "EXTRA_PYTHON_PACKAGES=\"${EXTRA_PYTHON_PACKAGES}\"" >> vars.sh
 # 00:01:17.010 /usr/local/bin/manylinux-entrypoint: line 8: /tmp/wheels/build-manylinux2014-wheels.sh: Permission denied
 chmod 755 build-manylinux2014-wheels.sh
 
-docker run -u root -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
+docker run --rm -u root -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
 
 # sort out wheel files for publishing
 
