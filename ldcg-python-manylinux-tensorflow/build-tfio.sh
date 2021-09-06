c83fda5c4ab19c05922a5178f449349050dca060
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2021-09-06 13:14:14 +0200

    ldcg-python-manylinux-tensorflow-io: fix script name
    Change-Id: I1c292b29dfc0bb36c0734fe83754f843396f4596

--- ldcg-python-manylinux-tensorflow/build-tfio.sh
+++ ldcg-python-manylinux-tensorflow/build-tfio.sh
@@ -17,4 +17,4 @@ if [ "$buildgit" = "true" ]; then
     echo '  - "git"' >> configs/ldcg-python-manylinux-tensorflow/ansible/vars/vars-tfio.yml
 fi
 
-docker run --rm -u root --security-opt seccomp=unconfined -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-manylinux2014-wheels.sh
+docker run --rm -u root --security-opt seccomp=unconfined -v $PWD:/tmp/workspace quay.io/pypa/manylinux2014_aarch64 /tmp/workspace/build-tfio-manylinux2014-wheels.sh
