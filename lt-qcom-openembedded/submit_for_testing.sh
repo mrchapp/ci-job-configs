ca49e8006a2928c05967cfa5331a89a4bb3cef8c
Aníbal Limón <anibal.limon@linaro.org>
2021-09-27 20:40:40 -0500

    lt-qcom-openembedded: Update submit_for_testing to use lava-test-plans
    In order to have common place for LAVA test-plans and drop duplicate CI ones.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I635c0fc2a8913e16fd32ae74be50a42ce35b954e

--- lt-qcom-openembedded/submit_for_testing.sh
+++ lt-qcom-openembedded/submit_for_testing.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-if [ -z "${DRY_RUN}" ]; then
-  rm -rf configs
-  git clone --depth 1 http://git.linaro.org/ci/job/configs.git
-  export CONFIG_PATH=$(realpath configs)
+set -ex
 
-  pip install --user --upgrade pip
-  pip install --user --upgrade setuptools
+# Create variables file to use with lava-test-plans submit_for_testing.py
+function create_testing_variables_file () {
+	cat << EOF > $1
+"LAVA_JOB_PRIORITY": "$LAVA_JOB_PRIORITY"
 
-  # Install jinja2-cli and ruamel.yaml, required by submit_for_testing.py
-  pip install --user ruamel.yaml.clib==0.2.2
-  pip install --user ruamel.yaml==0.16.13
-  pip install --user jinja2-cli
+"PROJECT": "projects/lt-qcom/"
+"PROJECT_NAME": "lt-qcom"
+"OS_INFO": "$OS_INFO"
+
+"BUILD_URL": "$BUILD_URL"
+"BUILD_NUMBER": "$BUILD_NUMBER"
+
+"DEPLOY_OS": "$DEPLOY_OS"
+"BOOT_URL": "$BOOT_URL"
+"BOOT_URL_COMP": "$BOOT_URL_COMP"
+"LXC_BOOT_FILE": "$LXC_BOOT_FILE"
+"ROOTFS_URL": "$ROOTFS_URL"
+"ROOTFS_URL_COMP": "$ROOTFS_URL_COMP"
+"LXC_ROOTFS_FILE": "$LXC_ROOTFS_FILE"
+
+"SMOKE_TESTS": "$SMOKE_TESTS"
+"WIFI_SSID_NAME": "LAVATESTX"
+"WIFI_SSID_PASSWORD": "NepjqGbq"
+"WLAN_DEVICE": "$WLAN_DEVICE"
+"WLAN_TIME_DELAY": "$WLAN_TIME_DELAY"
+"ETH_DEVICE": "$ETH_DEVICE"
+"PM_QA_TESTS": "$PM_QA_TESTS"
+EOF
+}
+
+rm -rf lava-test-plans
+if [ "$LAVA_TEST_PLANS_GIT_REPO" ]; then
+  git clone --depth 1 $LAVA_TEST_PLANS_GIT_REPO lava-test-plans
 else
-  export CONFIG_PATH=$(realpath ../)
+  git clone --depth 1 https://github.com/Linaro/lava-test-plans.git
 fi
+export LAVA_TEST_CASES_PATH=$(realpath lava-test-plans)
+pip3 install -r "$LAVA_TEST_CASES_PATH/requirements.txt"
 
 # main parameters
 export DEPLOY_OS=oe
 export OS_INFO=openembedded-${DISTRO}-${MANIFEST_BRANCH}
+export QA_SERVER_PROJECT=openembedded-rpb-${MANIFEST_BRANCH}
 
 # boot and rootfs parameters, BOOT_URL comes from builders.sh
 # and has not compression
@@ -32,61 +58,43 @@ case "${MACHINE}" in
     export DEVICE_TYPE="${MACHINE}"
 
     # Tests settings, thermal fails in db410c
-    export IGNORE_TESTS_REPO="https://git.linaro.org/landing-teams/working/qualcomm/configs.git"
-    export GST_IGNORE_TESTS_REPO=${IGNORE_TESTS_REPO}
-    export PIGLIT_IGNORE_TESTS_REPO=${IGNORE_TESTS_REPO}
     if [ ${DEVICE_TYPE} = "dragonboard-410c" ]; then
       export PM_QA_TESTS="cpufreq cpuidle cpuhotplug cputopology"
       export WLAN_DEVICE="wlan0"
       export WLAN_TIME_DELAY="0s"
       export ETH_DEVICE="eth0"
-      export GST_IGNORE_TESTS_FILE="qa/gst-validate/db410c.ignore"
-      export PIGLIT_IGNORE_TESTS_FILE="qa/piglit/db410c.ignore"
-
-      export BOOT_OS_PROMPT=\'root@dragonboard-410c:~#\'
     elif [ ${DEVICE_TYPE} = "dragonboard-820c" ]; then
       export PM_QA_TESTS="cpufreq cputopology"
       export WLAN_DEVICE="wlp1s0"
       export WLAN_TIME_DELAY="15s"
       export ETH_DEVICE="enP2p1s0"
-      export GST_IGNORE_TESTS_FILE="qa/gst-validate/db820c.ignore"
-      export PIGLIT_IGNORE_TESTS_FILE="qa/piglit/db820c.ignore"
-
-      export BOOT_OS_PROMPT=\'root@dragonboard-820c:~#\'
     elif [ ${DEVICE_TYPE} = "dragonboard-845c" ]; then
       export PM_QA_TESTS="cpufreq cpuidle cpuhotplug cputopology"
       export WLAN_DEVICE="wlan0"
       export WLAN_TIME_DELAY="15s"
       export ETH_DEVICE="enp1s0u3"
-      export GST_IGNORE_TESTS_FILE="qa/gst-validate/db410c.ignore"
-      export PIGLIT_IGNORE_TESTS_FILE="qa/piglit/db410c.ignore"
-
-      export BOOT_OS_PROMPT=\'root@dragonboard-845c:~#\'
     fi
     export SMOKE_TESTS="pwd, uname -a, ip a, vmstat, lsblk"
-    export PTEST_EXCLUDE="bluez5 libxml2 parted python strace"
 
+    create_testing_variables_file out/submit_for_testing.yaml
     case "${DISTRO}" in
       rpb)
         export ROOTFS_URL=${ROOTFS_SPARSE_BUILD_URL}
         export ROOTFS_URL_COMP="gz"
         export LXC_ROOTFS_FILE=$(basename ${ROOTFS_URL} .gz)
 
-	LAVA_TEMPLATE_PATH=${CONFIG_PATH}/lt-qcom/lava-job-definitions
-	cd ${LAVA_TEMPLATE_PATH}
-        python ${CONFIG_PATH}/openembedded-lkft/submit_for_testing.py \
+        cd lava-test-plans
+        ./submit_for_testing.py \
             --device-type ${DEVICE_TYPE} \
             --build-number ${BUILD_NUMBER} \
             --lava-server ${LAVA_SERVER} \
             --qa-server ${QA_SERVER} \
             --qa-server-team qcomlt \
-            --qa-server-project openembedded-rpb-${MANIFEST_BRANCH} \
-            --env-suffix="-${DISTRO}" \
-            --git-commit ${BUILD_NUMBER} \
-	    --template-path "${LAVA_TEMPLATE_PATH}" \
-	    --testplan-path "${LAVA_TEMPLATE_PATH}" \
-	    ${DRY_RUN} \
-            --test-plan testplan/main.yaml testplan/wifi.yaml testplan/bt.yaml
+            --qa-server-project ${QA_SERVER_PROJECT} \
+            --testplan-device-path projects/lt-qcom/devices \
+            ${DRY_RUN} \
+            --test-case testcases/distro-smoke.yaml testcases/bt.yaml testcases/wifi.yaml \
+            --variables ../out/submit_for_testing.yaml
       ;;
       rpb-wayland)
         echo "Currently no tests for rpb-wayland"
--- lt-qcom-openembedded/test_submit_for_testing.sh
+++ lt-qcom-openembedded/test_submit_for_testing.sh
@@ -2,22 +2,27 @@
 
 set -ex
 
-virtualenv --python=$(which python2) .venv
+mkdir -p out
+virtualenv --python=$(which python3) .venv
 source .venv/bin/activate
-pip install Jinja2 requests urllib3 ruamel.yaml==0.16.13
 
-export BUILD_NUMBER=125
+export LAVA_TEST_PLANS_GIT_REPO=https://github.com/alimon/lava-test-plans.git
+
+export BUILD_URL=https://ci.linaro.org/job/lt-qcom-openembedded-rpb-dunfell/451/
+export BUILD_NUMBER=451
 export DISTRO=rpb
-export MANIFEST_BRANCH=thud
+export MANIFEST_BRANCH=dunfell
 export QA_SERVER="http://localhost:8000"
 export QA_REPORTS_TOKEN="secret"
 export LAVA_SERVER=https://validation.linaro.org/RPC2/
 export DRY_RUN="--dry-run "
 
 export MACHINE=dragonboard-410c
-export BOOT_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/boot--4.14-r0-dragonboard-410c-20190409213001-125.img
-export ROOTFS_SPARSE_BUILD_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/rpb-console-image-test-dragonboard-410c-20190409213001-125.rootfs.img.gz
+export BOOT_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/dunfell/451/rpb/boot-apq8016-sbc--5.13-r0-dragonboard-410c-20210927115635-451.img
+export ROOTFS_SPARSE_BUILD_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/dunfell/451/rpb/rpb-console-image-test-dragonboard-410c-20210927115635-451.rootfs.img.gz
+
 bash submit_for_testing.sh
+cp -r lava-test-plans/tmp/dragonboard-410c out/
 
 # cleanup virtualenv
 deactivate
